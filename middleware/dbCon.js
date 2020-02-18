1c39f1171b75f527d8ff303d1830075ad27625f3
Akash Kumar Singh <akash.singh22396@gmail.com>
2020-02-18 14:15:11 +0530

    Update dbCon.js

--- middleware/dbCon.js
+++ middleware/dbCon.js
@@ -2,9 +2,9 @@ const mysql = require('mysql')
 
 exports.con = mysql.createConnection({
     host: '127.0.0.1',
-    user: 'root',
-    password: 'admin',
-    database: 'loginSystem', //'loginSystem',
+    user: '',
+    password: '',
+    database: '', //'',
     charset: 'utf8mb4'
 })
 console.log('database connected!')
